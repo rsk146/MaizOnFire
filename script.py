52b1fa1085e230b3ef9fdf8f5b93bed0adb22d80
Krishna Prajapati <kjprajapati18@users.noreply.github.com>
2021-02-07 17:52:21 -0500

    Finished BFS

--- script.py
+++ script.py
@@ -3,6 +3,7 @@ import numpy as np
 import os
 import sys
 import random
+from collections import deque
 
 #colors
 WHITE = (255, 255, 255) #Free = 0
@@ -51,8 +52,8 @@ def display_Maze(grid):
     pygame.display.flip()
 
 #dfs
-def generic_search_algo_fs(grid, fringe):
-    #fringe = [(0,0)]
+def dfs(grid):
+    fringe = [(0,0)]
     closed_set = set()
     parent = None
     while fringe:
@@ -82,6 +83,83 @@ def generic_search_algo_fs(grid, fringe):
                 grid[current[0]][current[1]] = 2
                 parent = current
         display_Maze(grid)
+    
+    print("Goal not reached")
+    return False
+
+def bfs(grid):
+    fringe = deque()
+    fringe.append((0,0))
+    closed_set = set()
+    parent = None
+    while fringe:
+        current = fringe.popleft()
+
+        if current == (dim-1, dim-1):
+            print("Goal Reached")
+            grid[current[0]][current[1]] = 7
+            return True
+        else:
+            if current not in closed_set:
+                if parent is not None: 
+                    grid[parent[0]][parent[1]] = 5
+                if(current[0] -1 >=0 and grid[current[0]-1][current[1]] != 1 and (current[0]-1, current[1]) not in closed_set):
+                    fringe.append((current[0]-1, current[1]))
+                    grid[current[0]-1][current[1]] = 4
+                if(current[1] -1 >=0 and grid[current[0]][current[1]-1] != 1 and (current[0], current[1]-1) not in closed_set):
+                    fringe.append((current[0], current[1]-1))
+                    grid[current[0]][current[1]-1] = 4
+                if(current[0] +1 < dim and grid[current[0]+1][current[1]] != 1 and (current[0]+1, current[1]) not in closed_set):
+                    fringe.append((current[0]+1, current[1]))
+                    grid[current[0]+1][current[1]] = 4
+                if(current[1] +1 < dim and grid[current[0]][current[1]+1] != 1) and (current[0], current[1]+1) not in closed_set:
+                    fringe.append((current[0], current[1]+1))
+                    grid[current[0]][current[1]+1] = 4
+                closed_set.add(current)
+                grid[current[0]][current[1]] = 2
+                parent = current
+        display_Maze(grid)
+    
+    print("Goal not reached")
+    return False
+
+
+#Not finished
+def a_star(grid):
+    fringe = deque()
+    fringe.append((0,0))
+    closed_set = set()
+    parent = None
+    while fringe:
+        current = fringe.popleft()
+
+        if current == (dim-1, dim-1):
+            print("Goal Reached")
+            grid[current[0]][current[1]] = 7
+            return True
+        else:
+            if current not in closed_set:
+                if parent is not None: 
+                    grid[parent[0]][parent[1]] = 5
+                if(current[0] -1 >=0 and grid[current[0]-1][current[1]] != 1 and (current[0]-1, current[1]) not in closed_set):
+                    fringe.append((current[0]-1, current[1]))
+                    grid[current[0]-1][current[1]] = 4
+                if(current[1] -1 >=0 and grid[current[0]][current[1]-1] != 1 and (current[0], current[1]-1) not in closed_set):
+                    fringe.append((current[0], current[1]-1))
+                    grid[current[0]][current[1]-1] = 4
+                if(current[0] +1 < dim and grid[current[0]+1][current[1]] != 1 and (current[0]+1, current[1]) not in closed_set):
+                    fringe.append((current[0]+1, current[1]))
+                    grid[current[0]+1][current[1]] = 4
+                if(current[1] +1 < dim and grid[current[0]][current[1]+1] != 1) and (current[0], current[1]+1) not in closed_set:
+                    fringe.append((current[0], current[1]+1))
+                    grid[current[0]][current[1]+1] = 4
+                closed_set.add(current)
+                grid[current[0]][current[1]] = 2
+                parent = current
+        display_Maze(grid)
+    
+    print("Goal not reached")
+    return False
 
 #main
 grid = []
@@ -103,8 +181,7 @@ pygame.display.set_caption("MaizOnFire")
 clock = pygame.time.Clock()
 done = False
 
-fringe = [(0,0)]
-if generic_search_algo_fs(grid, fringe):
+if bfs(grid):
     print("Found goal")
     while not done:
         display_Maze(grid)
