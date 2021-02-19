a5e5c85945c7c37e5b37a5301fc5356b8d157a11
Rakshay Kanthadai <rsk146@scarletmail.rutgers.edu>
2021-02-19 17:42:58 -0500

    comments and contributions added

--- script.py
+++ script.py
@@ -12,88 +12,120 @@ import time
 import copy
 import matplotlib.pyplot as plt
 
-
+##############Contributions: 
+# R above function indicates rsk146 majority contribution
+# K above function indicates kjp189 majority contribution
+# R/K or K/R indicates both wrote or edited it together and no individual can take majority credit
 
 ################## TO DO
-#
-#   WHY DOES LINE 132, 194, 259 CHECK IF NODE IN CLOSED??
-#   WHEN LINE 140, 202, 267 CHECKS THE SAME FREAKING THING??????
-#  
-#   WHY DO WE EVEN HAVE A CLOSED SET WHEN WE CAN JUST CHECK THE SQUARE COLOR?
-#
-#   -Add check to see if there is a path from fire to player
-#   -Maybe add another color to show path that player has taken already
-#  
-#
-#
+#   Report
 ###################
 
-
+#R
+#Generates a 2d array representation of a maze of dimension dim x dim 
+#each square has probability p to be a wall  
 def generate_Maze(dim, p):
+    #the final maze to be filled and outputted
     grid = []
+    #nested for loops to generate all elements up to size dim
     for row in range(dim):
         grid.append([])
         for column in range(dim):
+            #checks if a random number is less than p, which is true with probability p and false with probability 1-p
             if(random.uniform(0.,1.) < p):
+                #sets value to a wall
                 grid[row].append(sa.Status.WALL)
             else:
+                #sets value as a free square
                 grid[row].append(sa.Status.FREE)
-
+    #sets our start position at beginning
     grid[0][0] = sa.Status.PLAYER
+    #sets our goal position at the bottom right
     grid[dim-1][dim-1] = sa.Status.GOAL
     return grid
 
+#K
+#clears a maze of any squares that are visited or in the fringe by setting them to free
+#operates as a clean up function so we can rerun search algos on a maze after we run them once
 def clear_Search(grid):
     for row in range(dim):
         for col in range(dim):
+            #boolean to determine if current position in maze is visited or in the fringe
             clear = grid[row][col] == sa.Status.VISITED or grid[row][col] == sa.Status.FRINGE
-
+            #set back to free
             if(clear):
                 grid[row][col] = sa.Status.FREE 
+
+#K
+#clears the maze of any squares that are the path generated from a search algorithm
+#operates as a clean up function so we can rerun search algos on a maze after we run them once              
 def clear_Path(grid):
     for row in range(dim):
         for col in range(dim):
+            #if the maze value is considered a path, set it to be free
             if(grid[row][col] == sa.Status.PATH):
                 grid[row][col] = sa.Status.FREE
 
+#K/R
+#generates and returns a maze with a valid, random starting fire square added
+#valid means that the agent can reach the goal and the fire can reach the agent so he is in danger
+#takes in a dimension n and maze density p to generate the maze
 def add_valid_fire(n, p):
+    #boolean used for if fire added is valid
     valid = False
+    #generates a maze initially with wall density p and dimension n
     grid = generate_Maze(n, p)
+    #while we do not have a valid fire entry, keep trying
     while not valid:
+        #randomly generated coordinates for the fire's start position
         fireX = int(random.uniform(0, dim-1))
         fireY = int(random.uniform(0, dim-1))
-        #make sure fire not on player or exit
+        #make sure fire is not on player start or goal
         if((fireX != 0 or fireY != 0) and (fireX != dim-1 or fireY !=dim-1)):
+            #set those fire coordinates to a fire spot
             grid[fireX][fireY] = sa.Status.FIRE
+            #checks using dfs if it is possible to reach the end with this maze and fire square
+            #using dfs since it is fastest to find if a solution is possible
             fireGoalPath = sa.dfs(grid, 0, 0, n-1, n-1)
+            #clear the grid of non wall/fire squares so we can run another search
             clear_Path(grid)
             clear_Search(grid)
+            #find if there is a path from the start to the fire to see if there is a way the fire can reach the agent
             grid[fireX][fireY] = sa.Status.GOAL
+            #again, using dfs since it is fastest to find if a solution is possible
             firePath = sa.dfs(grid, 0, 0, fireX, fireY)
+            #if both paths can be found, the fire is valid. Otherwise, we regenerate the maze and try again
             if not (fireGoalPath and firePath):
                 grid = generate_Maze(n,p)
                 continue
             valid = True
+    #clears the grid from the last dfs
     clear_Search(grid)
     clear_Path(grid)
+    #sets the goal and the valid fire node
     grid[fireX][fireY] = sa.Status.FIRE
     grid[n-1][n-1] = sa.Status.GOAL
-    #print(str(fireGoalPath) + " " + str(firePath))
+    #returns the valid maze with valid fire square
     return grid, min(fireGoalPath, firePath)
 
+#K
+#spreads the fire to neighboring squares using the flammability q
 def spread_fire(grid, q):
-    #print(str(q))
+    #make a copy of the maze at this point so that we only look at fire squares that can spread this turn, not new ones that were already spread
     oldGrid = copy.deepcopy(grid)
     for row in range(dim):
         for col in range(dim):
             if(oldGrid[row][col] == sa.Status.FIRE or oldGrid[row][col] == sa.Status.WALL):
                 continue
-
             adjFire = count_fire(oldGrid, row, col)
+            #find the probability that this square is now on fire, based on the number of adjacent squares that are on fire
             pFire = 1 - (1-q)**adjFire
+            #set the value of this square to be on fire pFire% of the time
             if(random.uniform(0.,1.) < pFire):
                 grid[row][col] = sa.Status.FIRE
 
+#K
+#counts the number of adjacent squares that are on fire and returns that number
 def count_fire(oldGrid, row, col):
     count = 0
     for i in range(2):
@@ -104,15 +136,20 @@ def count_fire(oldGrid, row, col):
             else:
                 colCheck = col
                 rowCheck = row + j
+            #check if the indices in question are valid for the maze
             if(rowCheck >=0 and rowCheck < dim and colCheck >= 0 and colCheck < dim):
                 if(oldGrid[rowCheck][colCheck] == sa.Status.FIRE):
                     count = count + 1
     return count
 
+#K
+#performs strategy one, outlined in the report on a maze with a valid fire square, with flammability q
+#returns true if success, false if failure
 def performStrategyOne(grid, q):
     playerX = 0
     playerY = 0
     
+    #find the path from the start to the goal before spreading any fire
     if(not sa.a_star(grid, 0, 0, dim-1, dim -1)):
         #print("Not possible to escape the maiz")
         return False
@@ -120,11 +157,13 @@ def performStrategyOne(grid, q):
     clear_Search(grid)
     sa.display_Maze(grid)
     #time.sleep(0.5)
-
+    
     while (playerX,playerY) != (dim-1, dim-1):
+        #walk along the initial path, spreading the fire at each step
         (playerX, playerY) = path_step(grid, playerX, playerY)
         spread_fire(grid, q)
         sa.display_Maze(grid)
+        #check if agent runs into a fire square
         if(grid[playerX][playerY] == sa.Status.FIRE):
             #print("Player died to fire")
             return False
@@ -134,10 +173,13 @@ def performStrategyOne(grid, q):
     #print("Player escaped the MaizOfFire")
     return True
 
+#K
+#performs strategy two as outlined in the report
 def performStrategyTwo(grid, q):
     playerX = 0
     playerY = 0
     endDim = dim-1
+
     if(not sa.a_star(grid, 0, 0, endDim, endDim)):
         #print("Not possible to escape the maiz")
         return False
@@ -148,23 +190,24 @@ def performStrategyTwo(grid, q):
         sa.display_Maze(grid)
         #time.sleep(0.3)
         clear_Search(grid)
+        #take 1 step along the path found
         (playerX, playerY) = path_step(grid, playerX, playerY)
         clear_Path(grid)
         spread_fire(grid,q)
         if(grid[playerX][playerY] == sa.Status.FIRE):
             #print("Player died to fire")
             return False
-        
+        #recalculate the path after spreading the fire once
         if(not sa.a_star(grid, playerX, playerY, endDim, endDim)):
             #print("It is no longer possible to escape the maiz")
             return False
-        
     
     #print("Player escaped the MaizOfFire")
     return True
 
+#K
+#takes the player one step forward along the path squares on the grid. 
 def path_step(grid, row, col):
-    #Copy pasted from Fire checker cuz im lazy to check 4 nodes
     for i in range(2):
         for j in range(-1,2,2):
             if i == 0:
@@ -173,16 +216,17 @@ def path_step(grid, row, col):
             else:
                 colCheck = col
                 rowCheck = row + j
+            #checks if indices are valid for the maze
             if(rowCheck >=0 and rowCheck < dim and colCheck >= 0 and colCheck < dim):
                 if(grid[rowCheck][colCheck] == sa.Status.PATH or grid[rowCheck][colCheck] == sa.Status.GOAL):
                     grid[row][col] = sa.Status.FREE
                     grid[rowCheck][colCheck] = sa.Status.PLAYER
                     return (rowCheck,colCheck)
-
-    #print("yoo")
+    #returns initial position if there is no path squares adjacent
     return (row,col)
 
-
+#K
+#collects success rate data for DFS for a given number of trials for a variety of wall densities
 def collect_dfs_data(trials):
     count = 0
     print("Showing DFS Data Values with dim = " + str(dim) + ", trials = " + str(trials))
@@ -194,9 +238,10 @@ def collect_dfs_data(trials):
             if(sa.dfs(grid, 0, 0, dim-1, dim-1)):
                 count+=1
         print("P= " + str(p) + ", count = " + str(count))
-
     return
 
+#R
+#collects number of nodes explored difference data between bfs and a* for a particular maze for a given number of trials and a variety of wall densities
 def collect_bfs_astar_data(trials):
     print("Showing BFS and A* Data Values with dim = " + str(dim) + ", trials = " + str(trials))
     for prob in range(1,20):
@@ -205,44 +250,38 @@ def collect_bfs_astar_data(trials):
         for x in range(trials):
             grid = generate_Maze(dim,p)
             bfs_num_nodes_checked = sa.bfs(grid, 0,0, dim-1, dim-1)
-            #print("bfs nodes: " + str(bfs_num_nodes_checked))
             clear_Path(grid)
             clear_Search(grid)
             astar_num_nodes_checked = sa.a_star(grid, 0,0, dim-1, dim-1)
-            #print("a* nodes: " + str(astar_num_nodes_checked))
-            #if bfs_num_nodes_checked - astar_num_nodes_checked < 0:
-            #    print("CALL THE AMBULANCE")
             diff_total += bfs_num_nodes_checked - astar_num_nodes_checked
         print("P = " + str(p) + ", avg difference = " + str(diff_total/trials))
 
+#R
+#copies the path squares from one maze array into another
 def copyPath(finalGrid, pathGrid):
     for row in range(dim):
         for col in range(dim):
             if pathGrid[row][col] == sa.Status.PATH:
                 finalGrid[row][col] = sa.Status.PATH
 
-#on every path step if he reaches goal node return TRue and print success msg
+#R
+#performs strategy three as detailed in the report
 def performStrategyThree(grid, q, dist):
     playerX = 0
     playerY = 0
-    #add check in add valid fire
-    # if not sa.a_star(grid, playerX, playerY, dim-1, dim-1):
-    #         print("Not possible to escape maiz")
-    #         return False
-
-    #times = int(math.sqrt(dim)*q) 
+    #number of steps forward into the future we look
     times = 3
     while (playerX, playerY) != (dim-1, dim-1):
-        #this 3 might require some metric related thing lol
         clear_Path(grid)
         clear_Search(grid)
         while times > 0:
+            #copy the maze and spread the fire the appropriate number of times
             copyGrid = copy.deepcopy(grid)
             for i in range(times):
+                #spreads fire with 100% flammability in the copy of the maze
                 spread_fire(copyGrid, 1)
             sa.display_Maze(copyGrid)
-            #time.sleep(.5)    
-            #add check for if this a* fails
+            #checks if path is still possible, otherwise we drop the steps into the future we look and try again
             if not sa.a_star(copyGrid, playerX, playerY, dim-1, dim-1):
                 times -=1
                 if times == 0:
@@ -250,6 +289,8 @@ def performStrategyThree(grid, q, dist):
             else:
                 break
         sa.display_Maze(grid)
+        #if we can't go forward looking into the future with 100% flammability, see if we have a possible path at all
+        #if we dont, we fail. if we do, take a step along that path and try the algorithm again. 
         if times is 0:
             if not sa.a_star(grid, playerX, playerY, dim-1, dim-1):
                 #print("Failed to find a path out")
@@ -262,13 +303,12 @@ def performStrategyThree(grid, q, dist):
                 return False
             #time.sleep(.5)
             continue
+        #copy the path we found and walk along it the number of steps into the future we looked, while spreading the fire with the given flammability
         copyPath(grid, copyGrid)
-        #change to number of steps ahead we looked
         for i in range(times):
             (playerX, playerY) = path_step(grid, playerX, playerY)
             spread_fire(grid, q)
             sa.display_Maze(grid)
-            #time.sleep(.5)
             if(grid[playerX][playerY] == sa.Status.FIRE):
                 #print("Player died to fire")
                 return False
@@ -278,14 +318,16 @@ def performStrategyThree(grid, q, dist):
     #time.sleep(.5)
     return True
 
+#R
+#collects the success rate data per strategy. Each strategy returns a boolean so we count the number of trues for a variety of flammabilities and a wall density of .3
 def collect_strategy_data(trials, strategy):
     if strategy == 1:
         print("Showing Strategy One Data with dim = " + str(dim) + ", trials per flammability = " + str(trials))
-        for prob in range(1,20):
+        for prob in range(1,21):
             q = float(prob)/20
             successes = 0
             for x in range(trials):
-                grid = generate_Maze(dim, p2)
+                grid = generate_Maze(dim, .3)
                 while not sa.a_star(grid, 0, 0, dim-1, dim-1):
                     grid = generate_Maze(dim, p2)
                 clear_Search(grid)
@@ -297,11 +339,11 @@ def collect_strategy_data(trials, strategy):
             print("Flammability = " + str(q) + ", avg success rate = " + str(float(successes)/trials))
     elif strategy == 2:
         print("Showing Strategy Two Data with dim = " + str(dim) + ", trials per flammability = " + str(trials))
-        for prob in range(1,20):
+        for prob in range(1,21):
             q = float(prob)/20
             successes = 0
             for x in range(trials):
-                grid = generate_Maze(dim, p2)
+                grid = generate_Maze(dim, .3)
                 while not sa.a_star(grid, 0, 0, dim-1, dim-1):
                     grid = generate_Maze(dim, p2)
                 clear_Path(grid)
@@ -317,7 +359,7 @@ def collect_strategy_data(trials, strategy):
             q = float(prob)/20
             successes = 0
             for x in range(trials):
-                grid, minDist = add_valid_fire(dim, p2)
+                grid, minDist = add_valid_fire(dim, .3)
                 if performStrategyThree(grid, q, minDist):
                     successes+=1
             print("Flammability = " + str(q) + ", avg success rate = " + str(float(successes)/trials))
@@ -325,11 +367,12 @@ def collect_strategy_data(trials, strategy):
         print("Non valid strategy")
         return
 
+#R
+#plots the data we got from the collect data functions using matplotlib
 def genGraphs(graph):
     prb = []
     for i in range(0, 21):
         prb.append(float(i)/20)
-    #print mazeGenProb
     dfs = [10000, 9942, 9740, 9286, 8447, 7054, 5074, 2412, 190, 0, 0, 0,0,0,0,0,0,0,0,0, 0] #dim =100 trials 10000
     dfs_data = [x / 10000.0 for x in dfs]
     bfs_astar_data= [-1, 13, 60, 141, 270, 432, 536, 312, 14, 0,0,0,0,0,0,0,0,0,0,0, 0] #dim = 100, trials = 1000
@@ -338,14 +381,14 @@ def genGraphs(graph):
     strat3_data = [1, .93, .94, .81, .74, .57, .59, .41, .33, .16, .12, .05, 0, 0, .01, .01, 0,0,0,0,0] #dim =100 trials = 100
 
     if int(graph) == 4:
-        plt.plot(prb, dfs_data, color= 'g', linewidth = 4.0)
+        plt.plot(prb, dfs_data, color= 'c', linewidth = 4.0)
         plt.xlabel("Wall Density")
         plt.ylabel("DFS Success Rate")
         plt.title("DFS Success Rate vs Wall Density")
         plt.show()
 
     elif int(graph) == 5:
-        plt.plot(prb, bfs_astar_data, color= 'r', linewidth = 4.0)
+        plt.plot(prb, bfs_astar_data, color= 'm', linewidth = 4.0)
         plt.xlabel("Wall Density")
         plt.ylabel("Average Difference in BFS and A* Nodes Discovered")
         plt.title("Average Difference between BFS and A* Nodes Discovered For Different Wall Densities")
@@ -381,9 +424,11 @@ def genGraphs(graph):
         plt.ylim(0,1.0)
         plt.show()
 
-if(len(sys.argv) == 2):
+#main  area used to run the different functions for different purposes
+#usually take in command line arguments to do different things as the following commented code shows
+
+'''if(len(sys.argv) == 2):
     genGraphs(int(sys.argv[1]))
-#Do stuff in this loop
 else:
     import searchAlgos as sa
     dim = int(sys.argv[1])
@@ -395,15 +440,9 @@ else:
         q2 = float(sys.argv[3])
     else:
         q2 = 0
+    
+    grid = generate_Maze(dim, p2)
+    sa.display_Maze(grid)
+    time.sleep(10)
+'''
 
-
-
-# if(fireActive):
-#     grid, minDist = add_valid_fire(dim, p2)
-#     performStrategyThree(grid, q2, minDist)
-
-#collect_strategy_data(100, 3)
-
-#performStrategyOne(grid)
-#collect_dfs_data(10000)
-#collect_bfs_astar_data(1000)
--- searchAlgos.py
+++ searchAlgos.py
@@ -10,6 +10,8 @@ import math
 from enum import Enum
 import time
 
+#K
+#enumeration used to represent the different objects in a square of the maze
 class Status(Enum):
     PATH = 0
     GOAL = 1
@@ -20,7 +22,7 @@ class Status(Enum):
     WALL = 6
     FIRE = 7
 
-#colors
+#colors for our visualization to see the maze and traversals
 WHITE = (255, 255, 255) #Free 
 BLACK = (0,0,0)         #Walls 
 BLUE = (0, 0, 255)      #Player 
@@ -30,49 +32,57 @@ L_GREEN = (0, 255, 0)   #Visited
 YELLOW = (255, 255, 0)  #Fire 
 PURPLE = (255,0,255)    #Parent 
 
-# pygame.init()
-# size = [800, 800]
-# screen = pygame.display.set_mode(size)
-# pygame.display.set_caption("MaizOnFire")
-# clock = pygame.time.Clock()
+#R
+#initializations for the screen size, square size, etc. for the visualization
+pygame.init()
+size = [800, 800]
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("MaizOnFire")
+clock = pygame.time.Clock()
 dim = int(sys.argv[1])
 height = 800/dim-4  
 width = height
 margin = 4
 
-#show maze
+#R
+#calls to the pygame library to show the maze for it's different values in it's entries
 def display_Maze(grid):
-    # for event in pygame.event.get():
-    #     if event.type == pygame.QUIT:
-    #         sys.exit(0)
+    #check if exiting the visualization, then exit the program
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit(0)
+    #loop over all maze entries and set the color appropriately based on the enum value in the maze
+    for row in range(dim):
+            for column in range(dim):
 
-    # for row in range(dim):
-    #         for column in range(dim):
-
-    #             color = BLACK
-    #             if grid[row][column] == Status.FREE:
-    #                 color = WHITE
-    #             elif grid[row][column] == Status.PLAYER:
-    #                 color = BLUE
-    #             elif grid[row][column] == Status.GOAL:
-    #                 color = RED
-    #             elif grid[row][column] == Status.FRINGE:
-    #                 color = L_GREEN
-    #             elif grid[row][column] == Status.VISITED:
-    #                 color = GREEN
-    #             elif grid[row][column] == Status.FIRE:
-    #                 color = YELLOW
-    #             elif grid[row][column] == Status.PATH:
-    #                 color = PURPLE
-
-    #             pygame.draw.rect(screen, color, [(margin + width) * column + margin,
-    #                                             (margin+ height) * row + margin,
-    #                                             width, height])
-    # clock.tick(300)
-    # pygame.display.flip()
+                color = BLACK
+                if grid[row][column] == Status.FREE:
+                    color = WHITE
+                elif grid[row][column] == Status.PLAYER:
+                    color = BLUE
+                elif grid[row][column] == Status.GOAL:
+                    color = RED
+                elif grid[row][column] == Status.FRINGE:
+                    color = L_GREEN
+                elif grid[row][column] == Status.VISITED:
+                    color = GREEN
+                elif grid[row][column] == Status.FIRE:
+                    color = YELLOW
+                elif grid[row][column] == Status.PATH:
+                    color = PURPLE
+                #draw this square on the visualization with the appropriate color
+                pygame.draw.rect(screen, color, [(margin + width) * column + margin,
+                                                (margin+ height) * row + margin,
+                                                width, height])
+    #fps for updating the maze visually
+    clock.tick(60)
+    #update display
+    pygame.display.flip()
     return
 
-#show path
+#K
+#adds the squares that would be on the path taken upon reaching the end by backtracing through the squares and the parent grid
+#returns the length of the path
 def backtrace(maze, parentGrid, startX, startY, endX, endY):
     #Start at the goal node
     curX = endX
@@ -91,20 +101,23 @@ def backtrace(maze, parentGrid, startX, startY, endX, endY):
 
     return count
 
-
+#R
+#run depth first search on a maze, not allowed to land on squares that are not free
 def dfs(grid, startX, startY, endX, endY):
     startStatus = grid[startX][startY]
     endStatus = grid[endX][endY]
+    #fringe is a LIFO stack, using the python list and pop()
     fringe = [(startX, startY)]
+    #closed set to check if visited already
     closed_set = set()
-
+    #parent grid to see where we got to each square from
     parentGrid = []
     for row in range(dim):
         parentGrid.append([])
         for col in range(dim):
             parentGrid[row].append((0,0))
 
-    
+    #deltas for where we can go from a square. down, left, right, up
     directions = ((0,-1), (-1,0), (1,0),(0,1))
     
     while fringe:
@@ -116,7 +129,6 @@ def dfs(grid, startX, startY, endX, endY):
         #If we've reached the goal node, then trace the path and return
         if(curX == endX and curY == endY):
             pathLen = backtrace(grid, parentGrid, startX, startY, endX, endY)
-            #print(pathLen)
             grid[startX][startY] = startStatus
             grid[endX][endY] = endStatus
             return True
@@ -152,24 +164,29 @@ def dfs(grid, startX, startY, endX, endY):
     #print("Goal not reached")
     return False
 
+#R
+#run breadth first search on the maze, not allowed to land on squares that are not free
 def bfs(grid, startX, startY, endX, endY):
     startStatus = grid[startX][startY]
     endStatus = grid[endX][endY]
+    #fringe is a FIFO queue from queue library
     fringe = deque()
     fringe.append((startX, startY))
+    #closed set to check if visited already
     closed_set = set()
+    #counter for checking number of visited nodes
     counter = 0
-    
+    #parent grid to see where we got to each square from 
     parentGrid = []
     for row in range(dim):
         parentGrid.append([])
         for col in range(dim):
             parentGrid[row].append((0,0))
-
+    #deltas for where we can go from a square. down, left, right, up
     directions = ((0,-1), (-1,0), (1,0),(0,1))
     
     while fringe:
-        #Get x,y of next node to search from our stack
+        #Get x,y of next node to search from our queue
         current = fringe.popleft()
         counter +=1
         curX = current[0]
@@ -178,9 +195,9 @@ def bfs(grid, startX, startY, endX, endY):
         #If we've reached the goal node, then trace the path and return
         if(curX == endX and curY == endY):
             pathLen = backtrace(grid, parentGrid, startX, startY, endX, endY)
-            #print(pathLen)
             grid[startX][startY] = startStatus
             grid[endX][endY] = endStatus
+            #returns nodes visited
             return counter
         
         #Otherwise, search for next nodes to add to fringe
@@ -212,17 +229,22 @@ def bfs(grid, startX, startY, endX, endY):
         grid[curX][curY] = Status.VISITED
         display_Maze(grid)
     
+    #returns the number of nodes visited for failure as well. 
+    #Sometimes changed to return 0 if we care about success/failure instead of nodes visited
     return counter
+    #return 0
 
-#Uses Euclidean distance as heuristic
+#K
+#run a* using Euclidean distance as heuristic
 def a_star(grid, startX, startY, endX, endY):
+    #counter for number of nodes visited
     count = 0
     startStatus = grid[startX][startY]
     endStatus = grid[endX][endY]
-
+    #fringe is a heap from queue library
     fringe = PriorityQueue()
     beginning = sqrt((abs(endY-startY)-1)**2 + (abs(endX-startX)-1)**2)
-
+    #parent grid to see where we got to each square from
     parentGrid = []
     for row in range(dim):
         parentGrid.append([])
@@ -233,13 +255,13 @@ def a_star(grid, startX, startY, endX, endY):
     fringe.put((beginning, 0, startX, startY))
     closed_set = set()
 
+    #deltas for where we can go from a square. down, left, right, up
     directions = ((0,-1), (-1,0), (1,0),(0,1))
 
     while not fringe.empty():
 
         current = fringe.get()
         count+=1
-        #estimatedEndCost = current[0]
         curCost = current[1]
         curX = current[2]
         curY = current[3]
@@ -247,10 +269,9 @@ def a_star(grid, startX, startY, endX, endY):
 
         if(curX == endX and curY == endY):
             pathLen = backtrace(grid, parentGrid, startX, startY, endX, endY)
-            #print(pathLen)
             grid[startX][startY] = startStatus
             grid[endX][endY] = endStatus
-            return pathLen
+            return count
     
         for (dirX, dirY) in directions:
             #Get the next node (priority determined by order of "directions")
@@ -285,7 +306,10 @@ def a_star(grid, startX, startY, endX, endY):
         display_Maze(grid)
     
     #print("Goal not reached")
-    return 0
+    #returns the number of nodes explored even for failure
+    #sometimes returns 0 if we care about success/failure instead of nodes visited 
+    return count
+    #return 0
 
 
 #???????????? what thr fuck
